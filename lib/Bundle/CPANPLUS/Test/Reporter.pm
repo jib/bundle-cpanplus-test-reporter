1a1a832661aae7fb81c4ddad57adf61c0b781085
Jos Boumans <kane@cpan.org>
2006-11-01 07:50:40 +0000

    r2125@codebase2:  kane | 2006-08-17 22:07:45 +0200  Commit history from perforce during svk import:
     Change 11898 on 2005/01/21 by coral@cay
      
      	* update Test::Reporter dependency to 1.27 (fixed Win32 Notepad bug)
      
      Change 11772 on 2004/12/26 by coral@cay
      
      	* up bundle version to 0.03
      
      Change 11763 on 2004/12/26 by kane@myriad
      
      	   * this marks 0.02
      	   * update version
      
      Change 11758 on 2004/12/26 by coral@cay
      
      	* drop the Mail::Send darwin workaround, require Test::Reporter 1.21
      
      Change 11504 on 2004/12/04 by kane@myriad
      
      	* initial adding of a bundle for all test query/report modules
      
     
    
    
    git-svn-id: http://oss.dwim.org/bundle-cpanplus-test-reporter@908 4dccba1d-3c1b-0410-aec4-feb514a8dabc

--- lib/Bundle/CPANPLUS/Test/Reporter.pm
+++ lib/Bundle/CPANPLUS/Test/Reporter.pm
@@ -1,6 +1,6 @@
 package Bundle::CPANPLUS::Test::Reporter;
 
-$VERSION = "0.05";
+$VERSION = "0.04";
 
 1;
 
@@ -27,8 +27,6 @@ provide automated test reporting to C<testers.cpan.org>
 File::Temp
 
 HTTP::Request
-
-Net::DNS
 
 Net::SMTP
 
